46c5a5844ebc993d411250cf62f73cc224f9eac5
thejsj <jorge.silva@thejsj.com>
2017-03-15 16:13:47 -0700

    Step 10: Add script to generate docker client certs

---
+++ ansible/roles/docker_client/scripts/genClientCert.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [[ $1 = '' ]]; then
+  echo 'script requires a client name'
+  exit 1
+fi
+CLIENT=./files/certs/$1
+
+echo 'WARN: hard coded alpha-api-old gamma-services and beta-services for SWARM'
+if [[ $2 = '' ]]; then
+  echo 'script requires a client ip address'
+  exit 1
+fi
+
+mkdir $CLIENT
+
+# generate key for client
+openssl genrsa -out "$CLIENT/key.pem" 2048
+chmod 400 "$CLIENT/key.pem"
+
+# generate CSR for client
+openssl req \
+  -subj '/CN=client' \
+  -new \
+  -key "$CLIENT/key.pem" \
+  -out "$CLIENT/client.csr"
+
+chmod 400 "$CLIENT/client.csr"
+
+echo extendedKeyUsage=clientAuth,serverAuth > "$CLIENT/extfile.cnf"
+echo subjectAltName=IP:$2 >> "$CLIENT/extfile.cnf"
+
+# generate cert for client
+openssl x509 \
+  -req \
+  -days 365 \
+  -sha256 \
+  -in "$CLIENT/client.csr" \
+  -CA ca.pem \
+  -CAkey ca-key.pem \
+  -CAcreateserial \
+  -out "$CLIENT/cert.pem" \
+  -extfile "$CLIENT/extfile.cnf"
+
+# set permissions for deploy
+chmod 644 "$CLIENT/cert.pem"
+chmod 644 "$CLIENT/key.pem"
+
+# cleanup files we do not need
+rm $CLIENT/extfile.cnf
+rm -f $CLIENT/client.csr
