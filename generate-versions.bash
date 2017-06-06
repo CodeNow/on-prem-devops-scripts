8f2649de29b2a5e3bdbdbbcc271d4d1e547dbf9b
Jorge Silva <jorge.silva@thejsj.com>
2017-06-06 13:41:26 -0700

    Add cloudwatch reporting (#19)
    * Add swarm-cloudwatch-reporter
    
    * Update variables
    
    * Update branch stuff
    
    * Change name

--- generate-versions.bash
+++ generate-versions.bash
@@ -1,6 +1,6 @@
 JSON="{\"created\": \"$(date)\""
 
-for APP in astral agreeable-egret api arithmancy big-poppa clio cream detention docker-listener drake eru github-varnish ingress-proxy khronos link mongo-navi navi-proxy navi optimus palantiri pheidi prometheus-alerts prmmetheus rabbitmq redis registry sauron swarm-manager runnable-angular enterprise-signin; do
+for APP in astral agreeable-egret api arithmancy big-poppa clio cream detention docker-listener drake eru github-varnish ingress-proxy khronos link mongo-navi navi-proxy navi optimus palantiri pheidi prometheus-alerts prmmetheus rabbitmq redis registry sauron swarm-manager runnable-angular enterprise-signin furry-cactus; do
   GIT_BRANCH=`util::get_latest_tag $APP 2>/dev/null`
   if [[ -n "$GIT_BRANCH" ]]; then
     APP_NAME=$(sed 's#-#_#g' <<< $APP)
